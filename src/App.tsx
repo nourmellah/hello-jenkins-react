20cd48443ba7af650cccae3dedcccc0be7ba5b77
Nour Mellah <nour123mellah@gmail.com>
2025-12-19 21:07:22 +0100

    init

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+export default function App() {
+  // @ts-ignore
+  // @ts-ignore
+  return (
+    <div style={{ padding: 24 }}>
+      <h1>Hello World</h1>
+    </div>
+  );
+}
